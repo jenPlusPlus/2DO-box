5795b55f1e04aec723222ce6a8deb21609d1b369
Adam Mescher <admescher@gmail.com>
2017-07-17 17:53:19 -0600

    Add data object with given properties

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
+var state = [];
+
+function Idea() {
+  this.id = 0;
+  this.title = 0;
+  this.body = 0;
+  this.quality = 'swill';
+}
+
+Idea.prototype.generateID = function() {
+  this.id = 'id-' + Math.random().toString(36).substr(2,16);
+}
+
+Idea.prototype.setQuality = function() {
+  // some logic
+  // this.quality = 'genius';
+  // // some other logic
+  // this.quality = 'plausible';
+}
+
 // SAVE BUTTON EVENT LISTENER
 $('.top').on('click', '.idea-input-save-button', function(e) {
   e.preventDefault();
 
   var ideaInputTitle = $('.idea-input-title').val();
   var ideaInputBody = $('.idea-input-body').val();
+  var newIdea = new Idea();
+
+  newIdea.generateID();
+  newIdea.title = ideaInputTitle;
+  newIdea.body = ideaInputBody;
 
   $('.bottom').prepend(`
     <article class="idea-box">
       <div class="idea-box-top-line">
-        <h2 class="idea-box-header">${ideaInputTitle}</h2>
+        <h2 class="idea-box-header">${newIdea.title}</h2>
         <img class="idea-box-delete-button icon" src="images/delete.svg" alt="delete button" />
       </div>
-      <p class="idea-box-text">${ideaInputBody}</p>
+      <p class="idea-box-text">${newIdea.body}</p>
       <div class="idea-box-bottom-line">
         <img class="idea-box-upvote-button icon" src="images/upvote.svg" alt="upvote button" />
         <img class="idea-box-downvote-button icon" src="images/downvote.svg" alt="downvote button" />
@@ -20,6 +45,11 @@ $('.top').on('click', '.idea-input-save-button', function(e) {
     </article>
     `);
 
+    state.push(newIdea);
+
     $('.idea-input-title').val('');
     $('.idea-input-body').val('');
 })
+
+
+// http://www.frontcoded.com/javascript-create-unique-ids.html
