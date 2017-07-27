30415bffe0e1a5e5e8b2addae6f073e2d85ecd13
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-07-27 09:02:42 -0600

    fix styling

--- script.js
+++ script.js
@@ -34,10 +34,12 @@ $('.bottom').prepend(`
     </div>
     <p class="todo-box-text" contenteditable=true>${todo.body}</p>
     <div class="todo-box-bottom-line">
+      <div class="todo-box-pic-importance">
       <img class="todo-box-upvote-button icon" src="images/upvote.svg" alt="upvote button" tabindex="0"/>
       <img class="todo-box-downvote-button icon" src="images/downvote.svg" alt="downvote button" tabindex="0"/>
 
       <p class="todo-box-quality" tabindex="0">Importance: <span class="todo-box-quality-value">swill</span></p>
+      </div>
       <label><input type="checkbox" class="mark-as-completed" name="mark-as-completed" value="mark-as-completed">Mark as completed</label>
     </div>
   </article>
