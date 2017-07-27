63ba0e65b49f4bb949cdb016c52ce3cb7744d16c
Nick Teets <nickteets@gmail.com>
2017-07-27 07:31:13 -0600

    Clean up js comments and styling

--- script.js
+++ script.js
@@ -1,61 +1,11 @@
-/*  ------------------------------------------------------
-
-  ---------------------
-  - TABLE OF CONTENTS -
-  ---------------------
-
-  - On Page Load
-  - Event Listeners
-    1. Input Field Event Listener
-    2. Button Hover Event Listener
-      * Delete
-      * Upvote
-      * Downvote
-    3. Save Button Event Listener
-    4. Search Bar Event Listener
-    5. Delete Button Event Listener
-    6. Upvote Event Listener
-    7. Downvote Event Listener
-    8. Title Edit Event Listener
-    9. Body Edit Event Listener
-
-  - Functions
-    * createBox()
-    * pushtodoArrayToLocalStorage()
-    * pulltodoArrayFromLocalStorage()
-    * loadSavedtodos()
-
-*/
-
-// ON PAGE LOAD
-// look at localStorage functions to avoid global array variable
-
-// functions that still utilize global variable:
-// SEARCH BAR EVENT LISTENER
-// DELETE BUTTON EVENT LISTENER
-// UPVOTE BUTTON EVENT LISTENER
-// DOWNVOTE BUTTON EVENT LISTENER
-// BODY EDIT EVENT LISTENER
-// TITLE EDIT EVENT LISTENER
-
 populateDom();
 
-/* -------------------
-   - EVENT LISTENERS -
-   ------------------- */
+$(".todo-input-save-button").attr("disabled", true);
+$(".todo-input-title").keyup(enableSaveButton);
+$(".todo-input-body").keyup(enableSaveButton);
 
-   // make all of these named functions
-   // can we combine into a single function??? (Jen)
-
-// DISABLE SAVE BUTTON & INPUT FIELD EVENT LISTENERS
-  $(".todo-input-save-button").attr("disabled", true);
-  $(".todo-input-title").keyup(enableSaveButton);
-  $(".todo-input-body").keyup(enableSaveButton);
-
-// SAVE BUTTON EVENT
 $('.todo-input-save-button').on('click', saveInput);
 
-// SEARCH BAR EVENT LISTENER
 $('.search-bar-input').on('keyup', runSearch);
 $('#importanceFilter').on('change', filterByImportance);
 
@@ -74,8 +24,7 @@ function saveBody(e) {
   todo.body = $(e.target).closest('.todo-box-text').text();
   saveToLocalStorage(todo);
 }
-// edit html more javasciprt friendly also edit html class names
-// line 289 may need to change, if using array for quality
+
 function createBox (todo) {
 $('.bottom').prepend(`
   <article class="todo-box" data-id=${todo.id}>
@@ -94,7 +43,6 @@ $('.bottom').prepend(`
   </article>
   `);
   setQualityState(todo.id);
-  //move below to an addEventListenersToCard function
   $('[data-id='+todo.id+']').on('mouseover', '.todo-box-delete-button', deleteHoverOn);
   $('[data-id='+todo.id+']').on('mouseleave', '.todo-box-delete-button', deleteHoverOff);
   $('[data-id='+todo.id+']').on('mouseover', '.todo-box-upvote-button', upvoteHoverOn);
@@ -126,98 +74,86 @@ function updateMarkedAsCompleted (e) {
 function hideCardsOnDom(todo){
   if (todo.hidden === true) {
     $('[data-id='+todo.id+']').toggleClass('hidden')
-}
+  }
 }
 
 $('.show-completed').on('click', showCompletedTasks);
 
 function showCompletedTasks(e) {
-  var indexCardArray = [];
+  var cardArray = [];
   var objectKeys = Object.keys(localStorage);
   objectKeys.forEach(function (id) {
-    indexCardArray.push(JSON.parse(localStorage[id]));
+    cardArray.push(JSON.parse(localStorage[id]));
   });
-  indexCardArray.forEach(function(todo, i) {
+  cardArray.forEach(function(todo, i) {
    if (todo.completed === true && todo.hidden === true) {
-     createBox(todo);
-     $('[data-id='+todo.id+']').find('.mark-as-completed').attr('checked', true);
-// NT if this works these variables will have to be renamed
-// can e be moved into if and make the whoel thing a function
+    createBox(todo);
+    $('[data-id='+todo.id+']').find('.mark-as-completed').attr('checked', true);
     todo.hidden = false;
     saveToLocalStorage(todo);
-    //  $('.mark-as-completed').on('click', updateMarkedAsCompleted);
-
-// revisit this in the morning especiallt updateMarkedAsCompleted
-
-   }
- });
+    }
+  });
 }
 
- function saveInput(e){
-   e.preventDefault();
+function saveInput(e){
+  e.preventDefault();
 
-   var todoInputTitle = $('.todo-input-title').val();
-   var todoInputBody = $('.todo-input-body').val();
-   var newtodo = new todo(todoInputTitle, todoInputBody);
-   console.log(newtodo)
-   saveToLocalStorage(newtodo);
-   createBox(newtodo);
-
-
+  var todoInputTitle = $('.todo-input-title').val();
+  var todoInputBody = $('.todo-input-body').val();
+  var newtodo = new todo(todoInputTitle, todoInputBody);
+  console.log(newtodo)
+  saveToLocalStorage(newtodo);
+  createBox(newtodo);
   clearInputFields()
   focusOnTitle()
- }
+}
 
- function enableSaveButton(e){
-   if($(".todo-input-title").val() != "" && $(".todo-input-body").val() != ""){
-     $(".todo-input-save-button").attr("disabled", false);
-   }
- }
+function enableSaveButton(e){
+  if($(".todo-input-title").val() != "" && $(".todo-input-body").val() != ""){
+    $(".todo-input-save-button").attr("disabled", false);
+  }
+}
 
- function clearInputFields() {
-   $('.todo-input-title').val('');
-   $('.todo-input-body').val('');
- }
+function clearInputFields() {
+  $('.todo-input-title').val('');
+  $('.todo-input-body').val('');
+}
 
- function focusOnTitle() {
-   $('.todo-input-title').focus();
- }
+function focusOnTitle() {
+  $('.todo-input-title').focus();
+}
 
- function populateDom() {
-   var objectKeys = Object.keys(localStorage);
-   objectKeys.forEach(function (uniqueId) {
-     createBox(JSON.parse(localStorage[uniqueId]));
-   });
- }
+function populateDom() {
+  var objectKeys = Object.keys(localStorage);
+  objectKeys.forEach(function (uniqueId) {
+    createBox(JSON.parse(localStorage[uniqueId]));
+  });
+}
 
 
- function todo(title, body, importance) {
-   this.id = Date.now();
-   this.title = title;
-   this.body = body;
-   this.importance = importance || 2;
-   this.hidden = false;
-   this.completed = false;
- }
+function todo(title, body, importance) {
+  this.id = Date.now();
+  this.title = title;
+  this.body = body;
+  this.importance = importance || 2;
+  this.hidden = false;
+  this.completed = false;
+}
 
- function findObjectByKeyInLocalStorage(key) {
-   var todoFromLocalStorage = localStorage.getItem(key);
-   var parsedtodo = JSON.parse(todoFromLocalStorage);
-   return parsedtodo;
- }
+function findObjectByKeyInLocalStorage(key) {
+  var todoFromLocalStorage = localStorage.getItem(key);
+  var parsedtodo = JSON.parse(todoFromLocalStorage);
+  return parsedtodo;
+}
 
 function runSearch(e) {
-  var indexCardArray = [];
+  var cardArray = [];
   var objectKeys = Object.keys(localStorage);
   objectKeys.forEach(function (id) {
-    indexCardArray.push(JSON.parse(localStorage[id]));
+    cardArray.push(JSON.parse(localStorage[id]));
   });
-
-///NT create index card array function
-/// rename variables of this function
-
   var search = $(this).val().toUpperCase();
-  var searchedArray = indexCardArray.filter(function (newIndexCard) {
+  var searchedArray = cardArray.filter(function (newIndexCard) {
     return newIndexCard.title.toUpperCase().includes(search) || newIndexCard.body.toUpperCase().includes(search);
   });
   $('.bottom').empty();
@@ -227,19 +163,19 @@ function runSearch(e) {
 }
 
 function filterByImportance(e) {
-  var indexCardArray = [];
+  var cardArray = [];
   var objectKeys = Object.keys(localStorage);
   objectKeys.forEach(function (id) {
-    indexCardArray.push(JSON.parse(localStorage[id]));
+    cardArray.push(JSON.parse(localStorage[id]));
   });
   if($('#importanceFilter').val() == 5){
-    indexCardArray.forEach(function (task){
+    cardArray.forEach(function (task){
       createBox(task);
     });
   }
   else {
     $('.bottom').empty();
-    indexCardArray.forEach(function (card){
+    cardArray.forEach(function (card){
       if(card.importance == $('#importanceFilter').val()){
         createBox(card);
       }
@@ -247,72 +183,71 @@ function filterByImportance(e) {
   }
 }
 
- function deleteHoverOn(e) {
-   $(this).prop("src", "images/delete-hover.svg");
- }
+function deleteHoverOn(e) {
+  $(this).prop("src", "images/delete-hover.svg");
+}
 
- function deleteHoverOff(e) {
-   $(this).prop("src", "images/delete.svg");
- }
+function deleteHoverOff(e) {
+  $(this).prop("src", "images/delete.svg");
+}
 
- function upvoteHoverOn(e) {
-   $(this).prop("src", "images/upvote-hover.svg");
- }
+function upvoteHoverOn(e) {
+  $(this).prop("src", "images/upvote-hover.svg");
+}
 
- function upvoteHoverOff(e){
-   $(this).prop("src", "images/upvote.svg");
- }
+function upvoteHoverOff(e){
+  $(this).prop("src", "images/upvote.svg");
+}
 
- function downvoteHoverOn(e) {
-   $(this).prop("src", "images/downvote-hover.svg");
- }
+function downvoteHoverOn(e) {
+  $(this).prop("src", "images/downvote-hover.svg");
+}
 
- function downvoteHoverOff(e) {
-   $(this).prop("src", "images/downvote.svg");
- }
+function downvoteHoverOff(e) {
+  $(this).prop("src", "images/downvote.svg");
+}
 
- function deletetodo(e) {
-   var key = findCardKey(e);
-   findObjectByKeyInLocalStorage(key);
-   // make its own function
-   $(this).closest('article').remove();
-   localStorage.removeItem(key);
- }
+function deletetodo(e) {
+  var key = findCardKey(e);
+  findObjectByKeyInLocalStorage(key);
+  $(this).closest('article').remove();
+  localStorage.removeItem(key);
+}
 
- function upvotetodo(e) {
-   console.log("upvoting");
-   var key = findCardKey(e);
-   var todo = findObjectByKeyInLocalStorage(key);
-   todo.importance = todo.importance + 1;
+function upvotetodo(e) {
+  console.log("upvoting");
+  var key = findCardKey(e);
+  var todo = findObjectByKeyInLocalStorage(key);
+  todo.importance = todo.importance + 1;
     if(todo.importance > 4){
       todo.importance = 4;
     }
-   saveToLocalStorage(todo);
-   setQualityState(key);
- }
+  saveToLocalStorage(todo);
+  setQualityState(key);
+}
 
- function downvotetodo(e) {
-   var key = findCardKey(e);
-   var todo = findObjectByKeyInLocalStorage(key);
-   todo.importance = todo.importance - 1;
+function downvotetodo(e) {
+  var key = findCardKey(e);
+  var todo = findObjectByKeyInLocalStorage(key);
+  todo.importance = todo.importance - 1;
     if(todo.importance < 0){
       todo.importance = 0;
     }
-   saveToLocalStorage(todo);
-   setQualityState(key);
-   }
+  saveToLocalStorage(todo);
+  setQualityState(key);
+}
 
- function findCardKey(e) {
-   var todoCardId = $(e.target).closest("[data-id]").data("id");
-   return todoCardId;
- }
+function findCardKey(e) {
+  var todoCardId = $(e.target).closest("[data-id]").data("id");
+  return todoCardId;
+}
 
- function setQualityState(key) {
-   var todo = findObjectByKeyInLocalStorage(key);
-   var qualities = ['None', 'Low', 'Normal', 'High', 'Critical'];
+function setQualityState(key) {
+  var todo = findObjectByKeyInLocalStorage(key);
+  var qualities = ['None', 'Low', 'Normal', 'High', 'Critical'];
   $('[data-id='+todo.id+']').find('.todo-box-quality-value').text(qualities[todo.importance]);
- }
+}
 
- function saveToLocalStorage(todo) {
-   localStorage.setItem(todo.id, JSON.stringify(todo));
- }
+function saveToLocalStorage(todo) {
+  localStorage.setItem(todo.id, JSON.stringify(todo));
+}
