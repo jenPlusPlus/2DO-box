6e7e2777612b6d496159dbff28b9be5c4a502972
Nick Teets <nickteets@gmail.com>
2017-07-27 08:57:17 -0600

    Add styling to mark as completed checkbox

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ $('.bottom').prepend(`
       <img class="todo-box-downvote-button icon" src="images/downvote.svg" alt="downvote button" tabindex="0"/>
 
       <p class="todo-box-quality" tabindex="0">Importance: <span class="todo-box-quality-value">swill</span></p>
-      <label><input type="checkbox" class="mark-as-completed" name="mark-as-completed" value="mark-as-completed">Mark as completed</label>
+      <label><input type="checkbox" class="mark-as-completed" name="mark-as-completed" value="mark-as-completed"><span class="mark-completed-text">Mark as completed</span></label>
     </div>
   </article>
   `);
